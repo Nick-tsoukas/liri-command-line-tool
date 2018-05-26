607a8c4772888f5368279fbadcb86b25181695af
Nick Tsoukas <nick.tsoukas101@gmail.com>
2018-05-25 21:58:07 -0600

    Exploring the twitter API CLI ... ...

--- liri.js
+++ liri.js
@@ -1,12 +1,36 @@
 require("dotenv").config();
 
-var Twitter = require("twitter"),
-    Spotify = require('spotify-web-api-node'),
-    keys    = require("./keys"),
-    request = require("request"),
-    fs      = require("fs");
+var Twitter   = require("twitter"),
+    Spotify   = require('spotify-web-api-node'),
+    keys      = require("./keys"),
+    speak     = require("./speak")
+    request   = require("request"),
+    inquirer  = require("inquirer")
+    fs        = require("fs");
+
+var params = {
+    screen_name: 'NickTsoukas',
+    count: 20
+    }
+// constructor functions for twitter(client ie) and Spotify
+
+var client = new Twitter(keys.twitter);
+// var spotify = new Spotify(keys.spotify);
+
+// console.log(spotify._credentials.id + " is the id");
+// console.log(spotify._credentials.secret + 'this is the secret!!!!');
+
+function tweetPost(newTweet){
+  // takes "newTweet" and posts it to twitter account
+  client.post('statuses/update', {status: `${newTweet}`},  function(error, tweet, response){
+    if(error){
+      console.log(error);
+    }
+    console.log(tweet.text);  // Tweet body.
+  });
+};
 
 
 
- var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
+// calling the function will post tweets ... ...
+// tweetPost("Just testing program will stop posting ... ...")
---
+++ speak.js
@@ -0,0 +1,10 @@
+module.exports.tweet = function (newTweet) {
+  client.post('statuses/update', {status: `${newTweet}`},  function(error, tweet, response){
+    if(error){
+      console.log(error);
+    }
+    console.log(tweet.text);  // Tweet body.
+  });
+};
+// calling the function will post tweets ... ...
+// tweetPost("Just testing program will stop posting ... ...")
