8fb8bcee5e837add20a2d534db9ebb4174147a55
Nick Tsoukas <nick.tsoukas101@gmail.com>
2018-05-25 19:25:05 -0600

    setting up node and export import mods + files

--- liri.js
+++ liri.js
@@ -1,1 +1,12 @@
 require("dotenv").config();
+
+var Twitter = require("twitter"),
+    Spotify = require('spotify-web-api-node'),
+    keys    = require("./keys"),
+    request = require("request"),
+    fs      = require("fs");
+
+
+
+ var spotify = new Spotify(keys.spotify);
+// var client = new Twitter(keys.twitter);
