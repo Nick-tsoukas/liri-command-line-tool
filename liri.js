0a5e57be04a7e9754ea1b5f449afec31ae022870
Nick Tsoukas <nick.tsoukas101@gmail.com>
2018-05-27 13:16:56 -0600

    Adding the twitter cli functionality ... Done ... for now ...

--- liri.js
+++ liri.js
@@ -1,60 +1,110 @@
-require("dotenv").config(); // config env first #F \ \ \ \
+               require("dotenv").config(); // config env first ...
+               // ================================================
 
-const figlet = require('figlet');
-      clc    = require('cli-color');
-
-var   fs     = require('fs');
-
-const notice = clc.blue,
+const figlet = require('figlet'),
+      clc    = require('cli-color'),
+      notice = clc.blue,
       error  = clc.red.bold,
-      warn   = clc.yellow;
+      warn   = clc.yellow,
+      greenBright = clc.greenBright.bold;
+
+
+var   fs = require('fs'),
+      Twitter = require("twitter"),
+      Spotify = require('spotify-web-api-node'),
+      keysFile = require("./keys"),
+      speak = require("./speak"),
+      request = require("request"),
+      inquirer = require("inquirer"),
+      fs = require("fs");
+
+var params = {screen_name: 'NickTsouaks'};
+
+const template =
+`
+
+For twitter cli type node liri.js my-tweets
+
+  For Spotify cli type node liri.js play-song <song name>
+
+    For Movie Api cli type node liri.js my-tweets
+
+`
+
+
+// ====================Instuctions====================================
+    console.log(error('=================='), warn('Read Instructions'), error('========================'))
+    console.log(error('Nick and his Donkey were here! \n'));
+    console.log(greenBright(template));
+    console.log(error('====================================================================================================='));
+// ====================End_Of_Instructions============================
+
 
 figlet('HI + LADIES + GENTS !', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.log('Something went wrong...');
+    console.dir(err);
+    return;
+  }
+  console.log(data);
 });
 
-console.log(error('=================='), warn('Read Instructions'), error( '========================' ))
-console.log( error('Nick and dog was here!') );
-console.log( error('=========================================='));
-
-
-var Twitter   = require("twitter"),
-    Spotify   = require('spotify-web-api-node'),
-    keysFile  = require("./keys"),
-    speak     = require("./speak")
-    request   = require("request"),
-    inquirer  = require("inquirer")
-    fs        = require("fs");
 
 var client = new Twitter(keysFile.twitter);
 
-process.argv.forEach( (val,index) => {
-  console.log(`${index} : ${val}`);
+function buildTweets(){
+
+}
+
+process.argv.forEach((val, index) => {
+
+  if (process.argv[2] == 'my-tweets' && index === 2) {
+    // console.log(val, " : Lets get tweets : index", index);
+
+    figlet('tweets!', function(err, data) {
+      if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+      }
+      console.log(data);
+      console.log(error("=================================="))
+    });
+
+    function t(tweets){
+        tweets.forEach( (val,index) => {
+          if(index < 5) {
+            console.log(`${index + 1} `, "\n", greenBright(val.text), "\n");
+          }
+          return;
+        });
+    }
+
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        t(tweets);
+        // console.log(tweets[0]);
+      }
+});
+    // print 20 tweets from twitter api
+  }
+
+  // spot
+  if (process.argv[2] == 'play-song' && index === 2) {
+    console.log(process.argv[2]);
+  }
+
 });
 
-
-
-
-
-
-
-
 // function tweetPost(newTweet){
-//   // takes "newTweet" and posts it to twitter account
+//    takes "newTweet" and posts it to twitter account
 //   client.post('statuses/update', {status: `${newTweet}`},  function(error, tweet, response){
 //     if(error){
 //       console.log(error);
 //     }
-//     console.log(tweet.text);  // Tweet body.
+//     console.log(tweet.text);   Tweet body.
 //   });
 // };
-
-
 
 // var spotify = new Spotify(keys.spotify);
 
